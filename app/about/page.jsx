55b7f674018018c8fb93ea387c7c1b8cd00c72af
AvresCode <servas230@gmail.com>
2023-03-15 21:29:26 +0100

    Added tailwindCSS to the project

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div>
-      <h1>About page</h1>
+      <h1 className="text-lg py-2 m-4">About page</h1>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1> Hello world</h1>
+    <main>
+      <h1 className="text-3xl font-bold underline"> Hello world</h1>
     </main>
   );
 }
