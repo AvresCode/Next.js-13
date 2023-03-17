a8faac48f0fb5e42055d95c914b140fb7c876d9e
AvresCode <servas230@gmail.com>
2023-03-17 22:09:28 +0100

    fetched movie data

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
-import { Inter } from "next/font/google";
+export default async function Home() {
+  const moviesData = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
 
-const inter = Inter({ subsets: ["latin"] });
+  const response = await moviesData.json();
+  console.log(response);
 
-export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline"> Hello world</h1>
