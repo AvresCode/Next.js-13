f2eaf61a4845038ddb6e6272cbc1e0bfdb33273e
AvresCode <servas230@gmail.com>
2023-03-19 16:59:25 +0100

    modify link tag

--- app/Movie.js
+++ app/Movie.js
@@ -4,15 +4,16 @@ export default function Movie({ title, id, release_date, poster_path }) {
   const imagePath = "https://image.tmdb.org/t/p/original";
   return (
     <div>
-      <h1> {title}</h1>
-      <Link href={`/abc`}></Link>
-      <p>{release_date}</p>
-      <Image
-        src={imagePath + poster_path}
-        alt={title}
-        width={300}
-        height={500}
-      />
+      <Link href={`/abc`}>
+        <h1> {title}</h1>
+        <p>{release_date}</p>
+        <Image
+          src={imagePath + poster_path}
+          alt={title}
+          width={300}
+          height={500}
+        />
+      </Link>
     </div>
   );
 }
